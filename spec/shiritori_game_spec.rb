155d4ef9421409c0c80fcfd2a3990596eb51f11f
NakatsuboYusuke <>
2019-11-18 17:59:21 +0900

    [Fixed]step3

---
+++ spec/shiritori_game_spec.rb
@@ -0,0 +1,18 @@
+require './shiritori_game'
+
+RSpec.describe ShiritoriGame do
+  describe '.rule' do
+    it 'シリトリのルールが表示される' do
+      expect(ShiritoriGame.rule).to eq("すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n")
+    end
+  end
+
+  describe '.all_words' do
+    it { expect(ShiritoriGame.all_words).to eq ["リンゴ", "ゴリラ", "ラッパ", "パリ"] }
+  end
+
+  describe '#start' do
+    subject(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+    it { expect(shiritori_game.start).to include("\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n") }
+  end
+end

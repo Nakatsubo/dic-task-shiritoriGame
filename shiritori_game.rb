0ae70ef308443e2f95b4c9855d566d59dd9b203c
NakatsuboYusuke <>
2020-01-07 10:20:57 +0900

    update files

--- shiritori_game.rb
+++ shiritori_game.rb
@@ -1,8 +1,11 @@
 class ShiritoriGame
-  attr_accessor :player_name
+  attr_accessor :player_name, :histories
+
   def initialize(player_name)
     @player_name = player_name
+    @histories = []
   end
+
   def self.rule
     rule_text = ''
     rule_text << "すべてカタカナで入力してください\n"
@@ -10,10 +13,50 @@ class ShiritoriGame
     rule_text << "同じ単語を使ったら負けです\n"
     rule_text
   end
+
   def self.all_words
     %w[リンゴ ゴリラ ラッパ パリ]
   end
+
   def start
     "\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n"
   end
+
+  # getter
+  # def histories
+  #   @histories
+  # end
+
+  # setter
+  # def histories=(value)
+  #   @histories = value
+  # end
+
+  def computer_attack
+    if @histories.last == 'リンゴ'
+      return 'ゴリラ'
+    elsif @histories.last == 'パリ'
+      return 'リンゴ'
+    else
+      return nil
+    end
+  end
+
+  def validate_shiritori_rule(word)
+    if word == nil
+      return false
+    elsif word.split("").last == "ン"
+      return false
+    elsif word.include?("リンゴ") && histories.include?("リンゴ")
+      return false
+    elsif word.split("").first != histories.last.split("").last
+      return false
+    else word.split("").first == histories.last.split("").last
+      return true
+    end
+  end
+
+  # def mock_method
+  #   return true
+  # end
 end
--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -15,4 +15,175 @@ RSpec.describe ShiritoriGame do
     subject(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
     it { expect(shiritori_game.start).to include("\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n") }
   end
+
+  describe '#computer_attack' do
+    subject(:shiritori_game_computer_attack) { shiritori_game.computer_attack }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    context 'ShiritoriGame#histories 最後が リンゴ のとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+      it 'ゴリラが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('ゴリラ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が パリ のとき' do
+      before { shiritori_game.histories << 'パリ' }
+      it 'リンゴが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('リンゴ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が リンゴ, ゴリラ, ラッパ, パリ 以外のとき' do
+      before { shiritori_game.histories << 'イヌ' }
+
+      it 'nil が返ってくること' do
+        expect(shiritori_game_computer_attack).to eq(nil)
+      end
+    end
+  end
+
+  describe '#validate_shiritori_rule' do
+    subject(:shiritori_game_validate_shiritori_rule) { shiritori_game.validate_shiritori_rule(word) }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    context '引数の word が nil のとき' do
+      let(:word) { nil }
+      it { expect(shiritori_game_validate_shiritori_rule).to eq(false) }
+    end
+
+    context '引数の word の最後の文字が "ン" のとき' do
+      let(:word) { 'ライオン' }
+      it { expect(shiritori_game_validate_shiritori_rule).to eq(false) }
+    end
+
+    context '引数の word が ShiritoriGame#histories に含まれていたとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+      let(:word) { 'リンゴ' }
+      it { expect(shiritori_game_validate_shiritori_rule).to eq(false) }
+    end
+
+    context '引数の word の最初の文字が ShiritoriGame#histories の最後の文字と違うとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+      let(:word) { 'ラッパ' }
+      it { expect(shiritori_game_validate_shiritori_rule).to eq(false) }
+    end
+
+    context '引数の word の最初の文字が ShiritoriGame#histories の最後の文字と同じとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+      let(:word) { 'ゴリラ' }
+      it { expect(shiritori_game_validate_shiritori_rule).to eq(true) }
+    end
+  end
+
+  describe 'rspec practice' do
+    # mock_object という名前のモックオブジェクト（仮のオブジェクト）を作る
+    let(:mock_object) { double('mock_object') }
+
+    describe 'allow and receive' do
+      it 'mock_object オブジェクトから mock_method メソッドが呼ばれることを確認する' do
+        # mock_object というオブジェクトから mock_method というメソッドが呼ばれることを監視する
+        allow(mock_object).to receive(:mock_method)
+        # mock_object の mock_method を呼ぶ
+        mock_object.mock_method
+        # mock_object は、mock_method が呼べるオブジェクトであることを確認する
+        expect(mock_object).to have_received(:mock_method)
+        # mock_object は、mock_method が一度だけ呼ばれるオブジェクトであることを確認する
+        expect(mock_object).to have_received(:mock_method).once
+      end
+
+      it 'mock_object オブジェクトから mock_method メソッドが呼ばれると true を返すことを確認する' do
+        # mock_object というオブジェクトから mock_method というメソッドが呼ばれることを監視し、呼ばれたときは true を返す
+        allow(mock_object).to receive(:mock_method).and_return(true)
+        # mock_object オブジェクトから mock_method を呼び、結果が true であることを確認する
+        expect(mock_object.mock_method).to be_truthy
+      end
+    end
+
+    describe 'ARFG.gets' do
+      it 'ARFG オブジェクト から gets メソッドが呼ばれることを確認する' do
+        # ARFG オブジェクトから gets メソッドが呼ばれることを監視する
+        allow(ARGF).to receive(:gets)
+        # gets メソッドを呼ぶ
+        gets
+        # ARGF オブジェクトから gets メソッドが呼ばれることを確認する
+        expect(ARGF).to have_received(:gets)
+      end
+
+      it 'gets メソッドが呼ばれたとき、`げっつ！` という文字列が返ることを確認する' do
+        # ARFG オブジェクトから gets メソッドが呼ばれることを監視し、呼ばれたときは
+        allow(ARGF).to receive(:gets).and_return('げっつ！')
+        # gets メソッドを呼び出し、結果が `げっつ！` であることを確認する
+        expect(gets).to eq('げっつ！')
+      end
+    end
+  end
+
+  # puts の絡むテストが複雑になりがちだったので start だけじゃなく start_message とメソッドを分けました
+  describe '#start_message' do
+    subject(:shiritori_game_start_message) { shiritori_game.start_message }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    it { expect(shiritori_game_start_message).to eq(
+      "\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n\\_o< 先行しますか？ yes | no\n"
+    ) }
+  end
+
+  # 3歩目で書いた spec を更新します
+  describe '#start' do
+    subject(:shiritori_game_start) { shiritori_game.start }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    context 'yes の場合' do
+      let(:gets_text) { 'yes' }
+
+      it 'start_message が呼ばれること' do
+        allow(shiritori_game).to receive(:start_message)
+        allow(ARGF).to receive(:gets) { gets_text }
+        shiritori_game_start
+        expect(shiritori_game).to have_received(:start_message).once
+      end
+
+      it 'ShiritoriGame#attacker に player_name が設定されること' do
+        allow(ARGF).to receive(:gets) { gets_text }
+        shiritori_game_start
+        expect(shiritori_game.attacker).to eq('シリトリプレイヤー')
+      end
+
+      it '先攻メッセージが表示されること' do
+        allow(ARGF).to receive(:gets) { gets_text }
+        messages = ''
+        messages << "\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n"
+        messages << "\\_o< 先行しますか？ yes | no\n"
+        messages << "\\_o< 先攻ですね。単語を入力してください\n"
+        expect { shiritori_game_start }.to output(messages).to_stdout
+      end
+    end
+
+    context 'no (yes以外) の場合' do
+      let(:gets_text) { 'no' }
+
+      it 'start_message が呼ばれること' do
+        allow(shiritori_game).to receive(:start_message)
+        allow(ARGF).to receive(:gets) { gets_text }
+        shiritori_game_start
+        expect(shiritori_game).to have_received(:start_message).once
+      end
+
+      it "ShiritoriGame#attacker に 'computer' が設定されること" do
+        allow(ARGF).to receive(:gets) { gets_text }
+        shiritori_game_start
+        expect(shiritori_game.attacker).to eq('computer')
+      end
+
+      it '後攻メッセージが表示されること' do
+        allow(ARGF).to receive(:gets) { gets_text }
+        messages = ''
+        messages << "\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n"
+        messages << "\\_o< 先行しますか？ yes | no\n"
+        messages << "\\_o< 後攻ですね\n"
+        expect { shiritori_game_start }.to output(messages).to_stdout
+      end
+    end
+  end
 end
